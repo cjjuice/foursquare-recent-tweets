7f4c26a8ccf3b76b89b1133bc3135d123d474796
C.J. Bordeleau <bordeleau.cj@gmail.com>
2012-10-30 17:13:01 -0400

    change broken find_by! call

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -4,7 +4,7 @@ class CheckinsController < ApplicationController
   def push
     if params[:secret] == 'R3QCIQED4YOUISPQ0C4JU3GNWRNL0MDCYWHODCWGC1Z2DJKV'
       checkin = JSON.parse(params[:checkin])
-      user = User.find_by! uid: checkin["user"]["id"]
+      user = User.where uid: checkin["user"]["id"]
       recent_tweet = JSON.parse("http://search.twitter.com/search.json?q=#{["checkin"]["venue"]["name"]}&geocode=#{["checkin"]["venue"]["location"]["lat"]},#{["checkin"]["venue"]["location"]["lng"]},1mi&rpp=1")    
     
       reply = "https://api.foursquare.com/v2/checkins/#{checkin["id"]}/reply?text=#{recent_tweet["results"]["first"]["from_user_name"]} : #{recent_tweet["results"]["first"]["text"]}&oauth_token=#{user.oauth_token}"
