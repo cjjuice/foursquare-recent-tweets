0fca92a9c3b3e415fa83abeca57009d541d63465
C.J. Bordeleau <bordeleau.cj@gmail.com>
2012-10-30 17:16:13 -0400

    fix hash calls

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -5,7 +5,7 @@ class CheckinsController < ApplicationController
     if params[:secret] == 'R3QCIQED4YOUISPQ0C4JU3GNWRNL0MDCYWHODCWGC1Z2DJKV'
       checkin = JSON.parse(params[:checkin])
       user = User.where uid: checkin["user"]["id"]
-      recent_tweet = JSON.parse("http://search.twitter.com/search.json?q=#{["checkin"]["venue"]["name"]}&geocode=#{["checkin"]["venue"]["location"]["lat"]},#{["checkin"]["venue"]["location"]["lng"]},1mi&rpp=1")    
+      recent_tweet = JSON.parse("http://search.twitter.com/search.json?q=#{checkin["venue"]["name"]}&geocode=#{checkin["venue"]["location"]["lat"]},#{checkin["venue"]["location"]["lng"]},1mi&rpp=1")    
     
       reply = "https://api.foursquare.com/v2/checkins/#{checkin["id"]}/reply?text=#{recent_tweet["results"]["first"]["from_user_name"]} : #{recent_tweet["results"]["first"]["text"]}&oauth_token=#{user.oauth_token}"
 
