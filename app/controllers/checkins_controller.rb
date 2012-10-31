77d5aa3260694cecf779ae384db9470e2ba13ba4
C.J. Bordeleau <bordeleau.cj@gmail.com>
2012-10-31 17:59:42 -0400

    troubleshoot twitter gem

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -5,14 +5,13 @@ class CheckinsController < ApplicationController
     if params[:secret] == 'R3QCIQED4YOUISPQ0C4JU3GNWRNL0MDCYWHODCWGC1Z2DJKV'
       checkin = JSON.parse(params[:checkin])
       user = User.where uid: checkin["user"]["id"]
-      #recent_tweet = JSON.parse("http://search.twitter.com/search.json?q=#{checkin["venue"]["name"]}&geocode=#{checkin["venue"]["location"]["lat"]},#{checkin["venue"]["location"]["lng"]},1mi&rpp=1")   
  
-      Twitter.search(checkin["venue"]["name"], :count => 1 , :geocode => "#{checkin["venue"]["location"]["lat"]},#{checkin["venue"]["location"]["lng"]},1mi").results.map do |status|
-        recent_tweet = "#{status.from_user}:#{status.text}"
-      end
+      tweet = Twitter.search(checkin["venue"]["name"], :count => 1 , :geocode => "#{checkin["venue"]["location"]["lat"]},#{checkin["venue"]["location"]["lng"]},1mi").results.first
+      tweet_user = tweet.from_user
+      tweet_text = tweet.text
 
 
-      reply = "https://api.foursquare.com/v2/checkins/#{checkin["id"]}/reply?text=#{recent_tweet}&oauth_token=#{user.oauth_token}"
+      reply = "https://api.foursquare.com/v2/checkins/#{checkin["id"]}/reply?text=#{"#{tweet_user}:#{tweet_text}"&oauth_token=#{user.oauth_token}"
 
       render :json => reply  unless reply.empty?
     else
