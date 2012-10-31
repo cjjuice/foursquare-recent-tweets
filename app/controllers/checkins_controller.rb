a15b13fb4f43332d21818c1795dc2a8135a341b4
C.J. Bordeleau <bordeleau.cj@gmail.com>
2012-10-31 18:24:40 -0400

    troubleshooting

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -4,16 +4,16 @@ class CheckinsController < ApplicationController
   def push
     if params[:secret] == 'R3QCIQED4YOUISPQ0C4JU3GNWRNL0MDCYWHODCWGC1Z2DJKV'
       checkin = JSON.parse(params[:checkin])
-      user = User.where uid: checkin["user"]["id"]
+      user = User.where(uid: checkin["user"]["id"])
  
       tweet = Twitter.search(checkin["venue"]["name"], :count => 1 , :geocode => "#{checkin["venue"]["location"]["lat"]},#{checkin["venue"]["location"]["lng"]},1mi").results.first
       tweet_user = tweet.from_user
       tweet_text = tweet.text
 
 
-      reply = "https://api.foursquare.com/v2/checkins/#{checkin["id"]}/reply?text=#{"#{tweet_user}:#{tweet_text}"&oauth_token="#{user.oauth_token}"
+      reply = "https://api.foursquare.com/v2/checkins/#{checkin['id']}/reply?text=#{tweet_user}:#{tweet_text}&oauth_token=#{user.oauth_token}"
 
-      render :json => reply
+      render :json => reply unless reply.empty?
     else
       raise  "secret does not match"
     end
