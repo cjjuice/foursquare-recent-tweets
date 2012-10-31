b077b3c0799d6e8e11b19cbfa4767629512fe6c4
C.J. Bordeleau <bordeleau.cj@gmail.com>
2012-10-31 18:45:38 -0400

    add accessor to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :oauth_token
+  attr_accessible :oauth_token
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
