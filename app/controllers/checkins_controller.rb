2e30415952bef5baf3bb9fe6b74825857609c285
C.J. Bordeleau <bordeleau.cj@gmail.com>
2012-11-01 18:00:58 -0400

    add time to reply

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -6,10 +6,20 @@ class CheckinsController < ApplicationController
       checkin = JSON.parse(params[:checkin])
       user = User.where(uid: checkin["user"]["id"])
  
-      tweet = Twitter.search(checkin["venue"]["name"], :count => 1 , :geocode => "#{checkin["venue"]["location"]["lat"]},#{checkin["venue"]["location"]["lng"]},1mi").results.first
+      tweet = Twitter.search(checkin["venue"]["name"], :count => 1 , :geocode => "#{checkin["venue"]["location"]["lat"]},#{checkin["venue"]["location"]["lng"]},1mi", :result_type => "recent").results.first
+      tweet_time = ((Time.now - tweet.created_at)/60).round
       
+      
+      if tweet_time < 60 
+        tweet_time_text = "#{tweet_time} minutes ago"
+      elsif tweet_time >= 60 && tweet_time < 90  
+        tweet_time_text = "about 1 hour ago"
+      else
+        tweet_time_text = "about #{(tweet_time/60).round} hours ago"
+      end
+          
       if tweet != nil
-        tweet_text = "#{tweet.from_user} : #{tweet.text}" 
+        tweet_text = "@#{tweet.from_user} : #{tweet.text} - #{tweet_time_text}" 
       else
         tweet_text = "Sorry! No recent tweets from your location."
       end  
